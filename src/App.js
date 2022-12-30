8ef559cb700fbc17945a527ae287944b07a7f195
markojp <joakim.forthewin@outlook.com>
2022-12-30 19:45:31 +0100

    Generate list of monsters

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import { Component } from "react";
 class App extends Component {
   // ES7 class - no need for constructor
   state = {
-    monster1: { name: "Dracula" },
-    monster2: { name: "Frankenstein" },
-    monster3: { name: "Mummy" },
+    monsters: [
+      { name: "Dracula" },
+      { name: "Frankenstein" },
+      { name: "Mummy" },
+      { name: "Ghost" },
+    ],
   };
 
   render() {
-    return <div className="App">
-      <h2>{this.state.monster1.name}</h2>
-      <h2>{this.state.monster2.name}</h2>
-      <h2>{this.state.monster3.name}</h2>
-    </div>;
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1>{monster.name}</h1>
+        ))}
+      </div>
+    );
   }
 }
 
