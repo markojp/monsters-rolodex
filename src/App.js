def26fcb5c8e2e72f599d23811fe94306c635135
markojp <joakim.forthewin@outlook.com>
2022-12-30 19:19:12 +0100

    Initial project commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
   // ES7 class - no need for constructor
   state = {
-    name: { firstName: "Marco", lastname: "Polo" },
-    occupation: "explorer",
-  };
-
-  handleButtonClick = () => {
-    // ES7 function to keep this keyword correct
-    this.setState(
-      () => {
-        // can use (state, props)
-        return {
-          name: { firstName: "John", lastName: "Smith" },
-          occupation: "explorer",
-        };
-      },
-      () => {
-        // optional callback used after state updates
-        console.log(this.state);
-      }
-    );
+    monster1: { name: "Dracula" },
+    monster2: { name: "Frankenstein" },
+    monster3: { name: "Mummy" },
   };
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello! My name is {this.state.name.firstName}{" "}
-            {this.state.name.lastname} and I am an {this.state.occupation}.{" "}
-          </p>
-          <button onClick={this.handleButtonClick}>Change Text</button>
-        </header>
-      </div>
-    );
+    return <div className="App">
+      <h2>{this.state.monster1.name}</h2>
+      <h2>{this.state.monster2.name}</h2>
+      <h2>{this.state.monster3.name}</h2>
+    </div>;
   }
 }
 
