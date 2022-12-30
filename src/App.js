ec4e0828bfd8cf262a3470a186e9367acac0a2d9
markojp <joakim.forthewin@outlook.com>
2022-12-30 17:37:25 +0100

    Working setState by passing object, console log not showing lastest state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: "Marko",
+      name: {firstName: "Marco", lastname: "Polo"},
+      occupation:"explorer"
     };
   }
   render() {
@@ -14,10 +15,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello {this.state.name}</p>
+          <p>Hello! My name is {this.state.name.firstName} {this.state.name.lastname} and I am an {this.state.occupation}. </p>
           <button
             onClick={() => {
-              this.setState({name: 'Polo'})
+              this.setState({name: {firstName:'John', lastname: 'Smith'}, occupation: 'explorer', born: 'c 1580 Lincolnshire, England'})
               console.log(this.state);
             }}
           >
