d266f693f12050784b5f5df1560c8ef873885c84
markojp <joakim.forthewin@outlook.com>
2022-12-30 18:51:01 +0100

    proper setState change with function, ES7 class and buttonclickhandler

--- src/App.js
+++ src/App.js
@@ -3,27 +3,39 @@ import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: {firstName: "Marco", lastname: "Polo"},
-      occupation:"explorer"
-    };
-  }
+  // ES7 class - no need for constructor
+  state = {
+    name: { firstName: "Marco", lastname: "Polo" },
+    occupation: "explorer",
+  };
+
+  handleButtonClick = () => {
+    // ES7 function to keep this keyword correct
+    this.setState(
+      () => {
+        // can use (state, props)
+        return {
+          name: { firstName: "John", lastName: "Smith" },
+          occupation: "explorer",
+        };
+      },
+      () => {
+        // optional callback used after state updates
+        console.log(this.state);
+      }
+    );
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello! My name is {this.state.name.firstName} {this.state.name.lastname} and I am an {this.state.occupation}. </p>
-          <button
-            onClick={() => {
-              this.setState({name: {firstName:'John', lastname: 'Smith'}, occupation: 'explorer', born: 'c 1580 Lincolnshire, England'})
-              console.log(this.state);
-            }}
-          >
-            Change Text
-          </button>
+          <p>
+            Hello! My name is {this.state.name.firstName}{" "}
+            {this.state.name.lastname} and I am an {this.state.occupation}.{" "}
+          </p>
+          <button onClick={this.handleButtonClick}>Change Text</button>
         </header>
       </div>
     );
