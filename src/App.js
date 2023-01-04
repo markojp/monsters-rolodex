ee9865f445382dfe16a31950ac0073b479e85bba
markojp <joakim.forthewin@outlook.com>
2023-01-04 23:48:15 +0100

    Using lifecycle method componentDidMount to get data from JSON Placeholder

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import { Component } from "react";
 class App extends Component {
   // ES7 class - no need for constructor
   state = {
-    monsters: [
+    monsters: [],
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      });
+  }
+
+  /*
+
       { name: "Dracula", id: "123acde" },
       { name: "Frankenstein", id: "123wfwe" },
       { name: "Mummy", id: "123aeget" },
       { name: "Ghost", id: "1235rhb" },
-    ],
-  };
+*/
 
   render() {
     return (
