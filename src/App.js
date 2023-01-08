d2f253fa812b27318f0c72528c700e296d9296f9
markojp <joakim.forthewin@outlook.com>
2023-01-08 14:45:06 +0100

    Add searchbox, onChange event handler, filter list based on input

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Component } from "react";
 class App extends Component {
   // ES7 class - no need for constructor
   state = {
+    searchText: "",
     monsters: [],
   };
 
@@ -30,14 +31,38 @@ class App extends Component {
       { name: "Ghost", id: "1235rhb" },
 */
 
+  handleSearchInputChange = (event) => {
+    const newSearch = event.target.value;
+    //console.log(event.target.value);
+    this.setState((state) => {
+      return { searchText: newSearch };
+    });
+  };
+
   render() {
+    const searchMonsterName = this.state.searchText.toUpperCase();
+    const monsterList = this.state.monsters;
+    const monstersFound = monsterList.filter((monster) =>
+      monster.name.toUpperCase().startsWith(searchMonsterName)
+    );
+    console.log("mf: ", monstersFound);
+    const monstersFoundList = monstersFound.map((monster) => (
+      <div key={monster.id}>
+        <h1>{monster.name}</h1>
+      </div>
+    ));
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <div key={monster.id}>
-            <h1>{monster.name}</h1>
-          </div>
-        ))}
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          value={this.state.searchText}
+          onChange={(e) => this.handleSearchInputChange(e)}
+        />
+        <p>{this.state.searchText}</p>
+        {monstersFoundList}
       </div>
     );
   }
